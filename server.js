305c94120cdbf111fcadb20d0de356abda3bf5d8
Mohammed Mazahim <mazamim91@gmail.com>
2022-02-09 16:32:58 +1100

    heroku update1

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.use(express.static('public'));
 app.use('/api', api);
 
 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
